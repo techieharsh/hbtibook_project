a141759eb45742176a65775ffdbbf5f12cc149c8
techieharsh <techieharsh1991@gmail.com>
2015-07-07 12:32:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hbtibook::Application.config.secret_token = '404983ad2bfc6c4882af8af4f75654411a925245a3a80b0bfbab6143ee44c28caa6a65fab4cf3a72f608cd49a920161626bcc4f5ceb46e57952441188aa9b4dc'
